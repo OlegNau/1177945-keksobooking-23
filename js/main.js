3b3500ccdd8ace496c33a00224fe83f4b7967b65
OPNAUMOV <asler-72@mail.ru>
2021-05-31 22:29:59 +0300

    Поправил

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const getRandomValue = function (min, max) {
+const getRandomInteger = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-getRandomValue();
+getRandomInteger();
 
-function getRandomArbitrary(min, max, simbols) {
-  return (Math.random() * (max - min + 1) + min).toFixed(simbols);
+const getRandomFractionNumber = function (min, max, fractionalPart) {
+  return Number(Math.random() * (max - min + 1) + min).toFixed(fractionalPart);
 }
 
-getRandomArbitrary();
+getRandomFractionNumber();
