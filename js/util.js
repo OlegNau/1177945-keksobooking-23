11bcee977ca13dea8687fb9dc2b7bc3fba1c1050
OPNAUMOV <asler-72@mail.ru>
2021-06-14 19:20:35 +0300

    Модульность

---
+++ js/util.js
@@ -0,0 +1,11 @@
+const getRandomInteger = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getRandomFractionNumber = function (min, max, fractionalPart) {
+  return +((Math.random() * (max - min + 1) + min).toFixed(fractionalPart));
+};
+
+export{getRandomInteger, getRandomFractionNumber};
